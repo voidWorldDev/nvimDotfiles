41405615ef179e6844ff501cd82b64774e057570
voidMaster <pranaythapak.dev@gmail.com>
2025-11-29 17:54:50 +0530

    update: changes in markdown plugin

--- lua/kickstart/plugins/render-markdown.lua
+++ lua/kickstart/plugins/render-markdown.lua
@@ -1,14 +1,14 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
+	"OXY2DEV/markview.nvim",
 	ft = { "markdown" },
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
-		require("render-markdown").setup({
-			render_modes = { "n", "i", "v" }, -- render in normal, insert, and visual
+		require("markview").setup({
+			preview = {
+				enabled = true, -- live preview ON
+				update = "live", -- update on each keystroke
+			},
+			hybrid = true, -- shows both raw + rendered (like Obsidian)
 		})
-
-		-- Required so conceal works in insert mode
-		vim.opt.conceallevel = 2
-		vim.opt.concealcursor = "niv" -- show rendered text in insert + normal + visual
 	end,
 }
